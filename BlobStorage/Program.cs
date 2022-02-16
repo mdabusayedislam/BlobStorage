49d29dcfbb5e71e194405c1f64f6f0c24c534d7e
mdabusayedislam <abusayediit@gmail.com>
2022-02-16 19:49:59 +0600

    Add project files.

---
+++ BlobStorage/Program.cs
@@ -0,0 +1,45 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BlobStorage
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+            string connectionString = "Get Value from Access keys --Show keys-- Connection string";
+            Console.WriteLine("Blob Storage Demo");
+            ////Create
+            //BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            //BlobContainerClient blobContainerClient = await blobServiceClient.CreateBlobContainerAsync($"testcontainer");
+            //Console.WriteLine("Container Created");
+
+            ////Delete
+            //BlobContainerClient blobContainerClient = new BlobContainerClient(connectionString, "testcontainer");
+            //blobContainerClient.DeleteIfExists();
+
+            //Console.WriteLine("Container Deleted");
+
+            ////File Uploaded
+            //BlobContainerClient blobContainerClient = new BlobContainerClient(connectionString, "testcontainer");
+            //BlobClient blob = blobContainerClient.GetBlobClient(@"english/testazure.txt");
+            //using (FileStream file = File.OpenRead(@"E:\testlocal.txt"))
+            //{
+            //    await blob.UploadAsync(file);
+            //}
+
+            //Console.WriteLine("File Uploaded");
+
+            //File Name and Size Read
+            BlobContainerClient blobContainerClient = new BlobContainerClient(connectionString, "testcontainer");
+            await foreach (BlobItem blobItem in blobContainerClient.GetBlobsAsync())
+            {
+                Console.WriteLine($"File Name {blobItem.Name} Size {blobItem.Properties.ContentLength}");
+            }
+        }
+    }
+}
---
+++ TextAnalytics/Program.cs
@@ -0,0 +1,35 @@
+﻿using Azure;
+using Azure.AI.TextAnalytics;
+using System;
+
+namespace TextAnalytics
+{
+    class Program
+    {
+        private static readonly AzureKeyCredential credentials = new AzureKeyCredential("Put Key 1 Value");
+        private static readonly Uri endpoint = new Uri("Put End Point Value");
+        private static readonly TextAnalyticsClient client =new TextAnalyticsClient(endpoint, credentials);
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Please enter your text");
+            string InputText = Console.ReadLine();
+            var sentiment = GetSentiment(client, InputText);
+            Console.WriteLine($"Sentiment is {sentiment}");
+
+            var detectedLanguage = DetectLanguage(client, InputText);
+            Console.WriteLine($"Detected Language is {detectedLanguage}");
+        }
+        static string GetSentiment(TextAnalyticsClient client,String InputText)
+        {
+            DocumentSentiment documentSentiment = client.AnalyzeSentiment(InputText);
+            return documentSentiment.Sentiment.ToString();
+        }
+
+        static string DetectLanguage(TextAnalyticsClient client, String InputText)
+        {
+            DetectedLanguage detectedLanguage = client.DetectLanguage(InputText);
+            return detectedLanguage.Name.ToString();
+        }
+    }
+}
